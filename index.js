4cb620fb2a5f4fe282f81cd1c20e948199fd767b
JGWill-Orko <jgi@jgwill.com>
2021-06-15 07:40:13 -0400

    add:--help

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var fs   = require('fs'),
     args = require('optimist').argv,
     hbs  = require('handlebars');
 
+var argv = process.argv.slice(2);
+if (argv && argv[0] == "--help") 
+{console.log(`Usage:
+cat template.handlebars | gih --KEY1 VALUE1 --KEY2 VALUE2 > outfile.ext
+cat _render_TEMPLATE.sh.handlebars | gih --MODELNAME model_gia-ds-daliwill-210123-v02_new > render.sh `);
+process.exit(1)
+}
+
 if (args._.length) {
     try {
         args = JSON.parse(fs.readFileSync(args._[0]).toString());
